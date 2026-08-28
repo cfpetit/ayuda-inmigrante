644997d935d4cbf0d725dac4519534d798b33944
Orion <carlosfrancia@usal.es>
2026-08-28 11:23:01 +0200

    Mailing fix

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
+import socket
+
+# Force socket to resolve ONLY IPv4 (AF_INET) addresses on Render
+_orig_getaddrinfo = socket.getaddrinfo
+
+def _getaddrinfo_ipv4_only(*args, **kwargs):
+    responses = _orig_getaddrinfo(*args, **kwargs)
+    ipv4_responses = [res for res in responses if res[0] == socket.AF_INET]
+    return ipv4_responses if ipv4_responses else responses
+
+socket.getaddrinfo = _getaddrinfo_ipv4_only
 import os
 from app import create_app
 
